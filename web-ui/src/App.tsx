81d59b7da314bec48334f7c958bb27839853eaf2
Steven ott <stevenott@charter.net>
2024-04-12 13:46:59 -0500

    Added education information

--- web-ui/src/App.tsx
+++ web-ui/src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Education } from "./Education";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Toolbar />
+      <main className="flex min-h-screen flex-col items-center p-20">
+        <Education />
+        {/* <div className="flex flex-row shrink-0 p-6 gap-4 bg-zinc-900 rounded-xl"></div> */}
+      </main>
     </div>
+  );
+}
+
+interface toolbarProps {}
+
+export function Toolbar(props: toolbarProps) {
+  return (
+    <header className="bg-zinc-900 fixed top-0 w-full">
+      <nav className="container mx-auto px-6 py-3">
+        <div className="flex justify-evenly items-center font-serif">
+          <button>Experience</button>
+          <button>Education</button>
+          <button>Projects</button>
+        </div>
+      </nav>
+    </header>
   );
 }
 
---
+++ web-ui/src/Education.tsx
@@ -0,0 +1,163 @@
+export function Education() {
+  return (
+    <div className="grid gap-20 w-full text-left">
+      <EducationCard
+        title="Freshman"
+        years="2018 - 2019"
+        categories={[
+          {
+            title: "Degree-Specific",
+            courses: [
+              "Intro to Programming II",
+              "Programming at the HW/SW Interface",
+              "Essential Math for EE",
+              "ECE 1.0",
+            ],
+          },
+          {
+            title: "STEM-Focused",
+            courses: [
+              "Engineering Analysis & Problem Solving",
+              "Calculus Plus w/ Technology I",
+              "Calculus with Technology II",
+              "Physics by Inquiry I",
+              "University Physics I - Mechanics",
+            ],
+          },
+          {
+            title: "Humanities",
+            courses: ["Global Issues", "Introduction to Sociology"],
+          },
+        ]}
+      />
+      <EducationCard
+        title="Sophomore"
+        years="2019 - 2020"
+        categories={[
+          {
+            title: "Degree-Specific",
+            courses: [
+              "Electric Circuits I",
+              "Electric Circuits II and Lab",
+              "Digital Logic and Lab",
+              "Discrete Structures",
+              "Data Structures",
+            ],
+          },
+          {
+            title: "STEM-Focused",
+            courses: [
+              "Elementary Linear Algebra",
+              "Elementary Differential Equations",
+              "Physics by Inquiry II",
+              "University Physics II - Electricity & Magnetism",
+            ],
+          },
+          {
+            title: "Humanities",
+            courses: ["Beginning Acting", "Introduction to Literature"],
+          },
+        ]}
+      />
+      <EducationCard
+        title="Junior"
+        years="2020 - 2021"
+        categories={[
+          {
+            title: "Enterprise",
+            courses: ["Blue Marble Security"],
+          },
+          {
+            title: "Degree-Specific",
+            courses: [
+              "Computer Organization",
+              "Introduction to Algorithms",
+              "Electronics",
+              "Signals and Systems",
+              "Systems Programming",
+              "HW/SW System Integration",
+            ],
+          },
+          {
+            title: "STEM-Focused",
+            courses: [
+              "Engineering Statistics",
+              "Design Fundamentals",
+              "Statistical Programming and Analysis",
+            ],
+          },
+          {
+            title: "Humanities",
+            courses: ["Technical and Professional Communication", "Mythology"],
+          },
+        ]}
+      />
+      <EducationCard
+        title="Senior"
+        years="2021 - 2022"
+        categories={[
+          {
+            title: "Enterprise",
+            courses: ["Blue Marble Security", "Communication Contexts"],
+          },
+          {
+            title: "Degree-Specific",
+            courses: [
+              "Formal Models of Computation",
+              "Computer Systems Engineering & Performance",
+              "Computer Networks",
+              "Embedded System Interfacing",
+            ],
+          },
+          {
+            title: "STEM-Focus",
+            courses: [
+              "Understanding the Earth",
+              "Introduction to Archaeology",
+              "Electric Energy Systems",
+            ],
+          },
+        ]}
+      />
+    </div>
+  );
+}
+
+interface educationCategory {
+  title: string;
+  courses: string[];
+}
+
+interface educationCardProps {
+  title: string;
+  years: string;
+  categories: educationCategory[];
+}
+
+function EducationCard(props: educationCardProps) {
+  const { title, years, categories } = props;
+
+  return (
+    <div className="flex w-full gap-4">
+      <div className="w-1/4">
+        <h1 className="text-2xl font-serif underline">{title}</h1>
+        <h2 className="italic">{years}</h2>
+      </div>
+      <div className="grid outline rounded-xl p-6 gap-6 w-3/4 bg-teal-900">
+        {categories.map((category) => {
+          console.log(category);
+          return (
+            <div className="flex flex-col">
+              <h2 className="font-bold">{category.title}</h2>
+              <ul className="list-disc list-inside">
+                {category.courses.map((course, index) => (
+                  <li>{course}</li>
+                ))}
+              </ul>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
